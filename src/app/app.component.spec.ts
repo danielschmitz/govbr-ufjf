c890402acd375eff25b6b304910d95c1e2e5994d
Daniel Schmitz <danieljfa@gmail.com>
2025-06-30 11:01:50 -0300

    rename project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'meu-projeto-standalone' title`, () => {
+  it(`should have the 'govbr-ufjf' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('meu-projeto-standalone');
+    expect(app.title).toEqual('govbr-ufjf');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, meu-projeto-standalone');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, govbr-ufjf');
   });
 });
